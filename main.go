cd598ee55f9b9437cb3628999de79def82ddea89
life ⚡ <life@shellz.wtf>
2022-08-07 11:08:55 -0700

    Adds prompt from file input, and combine warning

--- main.go
+++ main.go
@@ -15,37 +15,78 @@ import (
 
 var (
 	apiKey             = os.Getenv("OPEN_AI_APIKEY")
-	flagUserInput      = flag.String("p", "", "prompt to send to gpt3 such as generate <lang> code to <something>")
-	flagPromptSuffix   = flag.String("e", "and end the content with //QED.", "suffix to append to the prompt")
-	flagIterations     = flag.Int("i", 1, "number of iterations to allow for content gen")
-	flagOutputfileName = flag.String("o", "", "Output file name")
 	flagStdIn          = flag.Bool("s", false, "Use stdin as input")
 	flagCombine        = flag.Bool("c", false, "Combine stdin and input prompt into one, stdin will be below the input prompt")
+	flagIterations     = flag.Int("i", 1, "number of iterations to allow for content gen")
+	flagUserInput      = flag.String("p", "", "prompt to send to gpt3 such as generate <lang> code to <something>")
+	flagPromptSuffix   = flag.String("e", "", "suffix to append to the prompt")
+	flagInputfileName  = flag.String("f", "", "input file to read from")
+	flagOutputfileName = flag.String("o", "", "Output file name")
 	prompt             string
 	err                error
 	stdinData          = ""
 )
 
 func init() {
+	// Parse the flags
 	flag.Parse()
+	// If the user input is not empty, use that as the prompt
+	var promptFromFile string
+	if *flagUserInput != "" {
+		prompt = *flagUserInput
+	}
+	// if we have data on stdin use the as the prompt
 	if *flagStdIn {
 		stdinData, err = getPipedData()
 		if err != nil {
 			log.Println(err)
 		}
 		prompt = stdinData
-
-	} else {
-		prompt = *flagUserInput
+	}
+	// if we have a file to read from, read it and use it as the prompt
+	if *flagInputfileName != "" {
+		promptFromFile, err = readInputFile()
+		if err != nil {
+			log.Println(err)
+		}
+		prompt = promptFromFile
 	}
 
-	if prompt == "" && stdinData == "" {
+	// if multiple input are given but combine is not set, combine them into one
+
+	if prompt == "" && stdinData == "" && promptFromFile == "" {
 		log.Fatal("Please provide a prompt")
 		flag.Usage()
 	}
 
+	inputFromParam := len(*flagUserInput) > 0
+	inputFromFile := len(*flagInputfileName) > 0
+	inputFromStdin := len(stdinData) > 0
+
+	// convert boolean to int
+	var inputFromParamInt int
+	if inputFromParam {
+		inputFromParamInt = 1
+	}
+	var inputFromFileInt int
+	if inputFromFile {
+		inputFromFileInt = 1
+	}
+	var inputFromStdinInt int
+	if inputFromStdin {
+		inputFromStdinInt = 1
+	}
+	shouldCombine := inputFromParamInt+inputFromFileInt+inputFromStdinInt > 1
+
+	// Write warning that combine flag was not set
+	if shouldCombine && !*flagCombine {
+		// print to error stream
+		// color the output orange
+		fmt.Fprintln(os.Stderr, "\033[33mWarning: combining input prompts is not set, only the last input prompt will be used")
+	}
+
 	if *flagCombine {
-		prompt = *flagUserInput + "\n:" + stdinData
+		prompt = *flagUserInput + "\n" + stdinData + "\n" + promptFromFile
 	}
 
 	// prompt = fmt.Sprintf("%s", *flagUserInput)
@@ -82,8 +123,22 @@ func getPipedData() (pipedData string, err error) {
 	return pipedData, err
 }
 
-func main() {
+func readInputFile() (inputPrompt string, err error) {
+	inputFile, err := os.Open(*flagInputfileName)
+	if err != nil {
+		return "", err
+	}
+	defer inputFile.Close()
 
+	scanner := bufio.NewScanner(inputFile)
+	for scanner.Scan() {
+		inputPrompt += scanner.Text()
+	}
+
+	return inputPrompt, nil
+}
+
+func main() {
 	iterations := *flagIterations
 	inputPrompt := prompt
 	var newContent string
@@ -103,7 +158,7 @@ func main() {
 		// Send the prompt to the API
 		inputPrompt, newContent = gpt3.SendOpenAIPrompt(inputPrompt)
 		// Print the generated content
-		fmt.Println(newContent)
+		fmt.Println("\033[96m", newContent)
 	}
 
 	if *flagOutputfileName != "" {
